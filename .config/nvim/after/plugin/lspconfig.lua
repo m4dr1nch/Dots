c8ee2fbea87e0cc8331ab852c9c5dfc16bcd1cac
martinss <martinss@pwcits.lv>
2024-02-07 08:55:17 +0200

    Changed python LSP for nvim.

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -3,7 +3,7 @@ PLUGIN: lspconfig
 --]]
 local lspconfig = require('lspconfig')
     lspconfig.phpactor.setup {underline=false}
-    lspconfig.pyright.setup {}
+    lspconfig.pylsp.setup {}
     lspconfig.rust_analyzer.setup {
         settings = {
             ['rust-analyzer'] = {},
